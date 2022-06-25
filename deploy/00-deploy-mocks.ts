26a4577baba6ccb499159c9d210ec4c958854c1a
nikmace <nikmacentosh@gmail.com>
2022-06-25 13:20:17 +0300

    Deploy scripts both for local networks &  rinkeby testnet

---
+++ deploy/00-deploy-mocks.ts
@@ -0,0 +1,42 @@
+import { HardhatRuntimeEnvironment } from "hardhat/types";
+import { DeployFunction } from "hardhat-deploy/types";
+import { ethers } from "hardhat";
+
+import { developmentChains } from "../helper-hardhat-config";
+
+const BASE_FEE = ethers.utils.parseEther("0.25");
+const GAS_PRICE_LINK = 1e9;
+
+const deployMocks: DeployFunction = async function deployFunc(
+  hre: HardhatRuntimeEnvironment
+) {
+  const { deployments, getNamedAccounts, network } = hre;
+  const { deploy, log } = deployments;
+  const { deployer } = await getNamedAccounts();
+  const chainId = network.config.chainId;
+
+  const args = [BASE_FEE, GAS_PRICE_LINK];
+
+  if (developmentChains.includes(network.name)) {
+    log("Local network detected! Deploying mocks...");
+    await deploy("VRFCoordinatorV2Mock", {
+      contract: "VRFCoordinatorV2Mock",
+      from: deployer,
+      log: true,
+      args,
+    });
+    log("Mocks deployed!");
+    log("--------------------------------------------");
+
+    log(
+      "You are deploying to a local network, you'll need a local network running to interact"
+    );
+    log(
+      "Please run `yarn hardhat console --network localhost` to interact with the deployed smart contracts!"
+    );
+    log("----------------------------------");
+  }
+};
+
+export default deployMocks;
+deployMocks.tags = ["all", "mocks"];
---
+++ deploy/01-deploy-raffle.ts
@@ -0,0 +1,90 @@
+import { ethers } from "hardhat";
+import { HardhatRuntimeEnvironment } from "hardhat/types";
+import { DeployFunction } from "hardhat-deploy/types";
+
+import { developmentChains, networkConfig } from "../helper-hardhat-config";
+import { VRFCoordinatorV2Mock } from "../typechain";
+import verify from "../utils/verify";
+
+const VRF_SUB_FUND_AMOUNT = ethers.utils.parseEther("2");
+
+const deployFundMe: DeployFunction = async function (
+  hre: HardhatRuntimeEnvironment
+) {
+  const { getNamedAccounts, deployments, network } = hre;
+  const { deploy, log } = deployments;
+  const { deployer } = await getNamedAccounts();
+
+  let vrfCoordinatorV2Address;
+  let subscriptionId;
+
+  if (developmentChains.includes(network.name)) {
+    const vrfCoordinatorV2Mock: VRFCoordinatorV2Mock = await ethers.getContract(
+      "VRFCoordinatorV2Mock"
+    );
+
+    vrfCoordinatorV2Address = vrfCoordinatorV2Mock.address;
+
+    const txResponse = await vrfCoordinatorV2Mock.createSubscription();
+    const txReceipt = await txResponse.wait(1);
+    // Get the subscription ID and fund the subscription
+    // @ts-ignore
+    subscriptionId = ethers.BigNumber.from(
+        // @ts-ignore
+        txReceipt.events[0].topics[1]
+    );
+
+    await vrfCoordinatorV2Mock.fundSubscription(
+      subscriptionId,
+      VRF_SUB_FUND_AMOUNT
+    );
+  } else {
+    vrfCoordinatorV2Address = networkConfig[network.name].vrfCoordinatorV2;
+    subscriptionId = networkConfig[network.name].subscriptionId;
+  }
+  //   address vrfCoordinatorV2, // Contract address
+  //     uint256 entraceFee,
+  //     bytes32 gasLane,
+  //     uint64 subscriptionId,
+  //     uint32 callbackGasLimit,
+  //     uint256 interval
+  const entraceFee = networkConfig[network.name].entranceFee;
+  const gasLane = networkConfig[network.name].gasLane;
+  const callBackGasLimit = networkConfig[network.name].callBackGasLimit;
+  const interval = networkConfig[network.name].interval;
+
+  const args = [
+    vrfCoordinatorV2Address,
+    subscriptionId,
+    gasLane,
+    interval,
+    entraceFee,
+    callBackGasLimit,
+  ];
+
+  try {
+    const raffle = await deploy("Raffle", {
+      from: deployer,
+      args,
+      log: true,
+      waitConfirmations: networkConfig[network.name].blockConfirmations || 1,
+    });
+
+    log(`Raffle deployed at ${raffle.address}`);
+
+    if (
+      !developmentChains.includes(network.name) &&
+      process.env.ETHERSCAN_API_KEY
+    ) {
+      log("Verifying contract... please wait...");
+      await verify(raffle.address, args);
+    }
+  } catch (e) {
+    console.log(e);
+  }
+
+  log("----------------------------------------");
+};
+
+export default deployFundMe;
+deployFundMe.tags = ["all", "Raffle"];
